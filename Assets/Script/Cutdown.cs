c7969f87514e2dd8e6279636d3379fe2bcd47001
RizkyHendraSaputra <71587872+RizkyHendra@users.noreply.github.com>
2021-11-26 13:11:48 +0700

    Cutdown dan Gameover jika habis

---
+++ Assets/Script/Cutdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cutdown : MonoBehaviour
+{
+    float currentTime1;
+    public float startingTime1 = 10f;
+    [SerializeField] Text cutdownText;
+    void Start()
+    {
+        currentTime1 = startingTime1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime1 += 1 * Time.deltaTime;
+        cutdownText.text = currentTime1.ToString("0");
+        if(currentTime1 >= 0)
+        {
+            currentTime1 = 0;
+        }
+    }
+}
