73bde35ddb327de36b27c30afb04bb6dcd6f1196
rezelion <ahmadspade2014@gmail.com>
2021-12-04 00:08:10 +0700

    Added Door Mechanized (LivingRoom Scenes)
    To Open the door Press E , To close the door press F

---
+++ Assets/Script/Scene Script/DoorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject DoorCollider;
+
+    public void OpenDoor()
+    {
+        DoorCollider.GetComponent<BoxCollider2D>().enabled = true;
+        gameObject.SetActive(false);
+    }
+    public void CloseDoor()
+    {
+        DoorCollider.GetComponent<BoxCollider2D>().enabled = false;
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Script/Scene Script/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private DoorController DoorEnabled;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            DoorEnabled.OpenDoor();
+        }
+        if (Input.GetKeyDown("f"))
+        {
+            DoorEnabled.CloseDoor();
+        }
+    }
+}
