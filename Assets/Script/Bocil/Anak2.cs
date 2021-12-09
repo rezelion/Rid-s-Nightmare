0d666fcadad77ad20adb0273a808ee12102a3330
Farid Taufik <90214234+faridtaufik@users.noreply.github.com>
2021-12-10 00:23:23 +0700

    UpdateLvl1_Alternates

--- Assets/Script/Bocil/Anak2.cs
+++ Assets/Script/Bocil/Anak2.cs
@@ -5,21 +5,9 @@ using UnityEngine;
 
 public class Anak2 : Collecteble
 {
-    public static Anak2 Instance { get; private set; }
     [SerializeField] int AnakValue = 1;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+    
     protected override  void Collected()
     {
         GameManager.Myinstance.AddAnak(AnakValue);
