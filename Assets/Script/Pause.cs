be33a31b100ec206fc2af886880c6b63a67b12f0
RizkyHendraSaputra <71587872+RizkyHendra@users.noreply.github.com>
2021-12-15 15:13:05 +0700

    Ui Change

---
+++ Assets/Script/Pause.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause : MonoBehaviour
+{
+    public static bool GamePause = false;
+    public GameObject pauseMenu;
+
+    //public GameObject StopGame;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePause)
+            {
+                Resume();
+            }
+            else
+            {
+                pausee();
+            }
+        }
+    }
+
+    public void ButtonPause()
+    {
+        if (GamePause)
+        {
+            Resume();
+        }
+        else
+        {
+            pausee();
+        }
+    }
+
+    public void Resume()
+    {
+        // StopGame.SetActive(false);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePause = false;
+    }
+
+    public void pausee()
+    {
+        // StopGame.SetActive(true);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Restart()
+    {
+        pauseMenu.SetActive(false);
+        GamePause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
