67383ed07f512371811052ede8ce464278e37f3c
Rizky Hendra Saputra <71587872+RizkyHendra@users.noreply.github.com>
2022-01-13 23:43:57 -0800

    beta

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -38,6 +38,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
     public void PilihLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
